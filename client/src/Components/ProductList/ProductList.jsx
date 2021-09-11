007a30fb3497f18350d5af0ec0c41c04372d9fb4
FranciscoPink <franciscorosadomunoz@icloud.com>
2021-09-11 11:36:59 -0400

    Created Services folder with axios requests

---
+++ client/src/Components/ProductList/ProductList.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+import Detail from '../Detail/Detail'
+import { getProducts } from '../../services/products'
+
+const ProductCards = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const allProducts = await getProducts()
+      setProducts(allProducts)
+    }
+    fetchProducts()
+  }, [])
+
+  const CARDS = products
+    .reverse()
+    .map((product, index) =>
+      index < 8 ? (
+        <ProductCard
+          _id={product._id}
+          name={product.name}
+          imgURL={product.imgURL}
+          key={index}
+        />
+      ) : null
+    )
+
+  return (
+    <div className='product-cards'>
+      <div className='latest'>LATEST</div>
+      <div className='cards'>{CARDS}</div>
+    </div>
+  )
+}
+
+export default ProductCards
