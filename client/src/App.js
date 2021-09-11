830734a840b18b415a24db60bc794bf81bdb16b7
FranciscoPink <franciscorosadomunoz@icloud.com>
2021-09-11 12:48:34 -0400

    Added Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import './App.css'
+import Home from './screens/Home/Home'
+import Products from './screens/Products/Products'
+import ProductCreate from './screens/ProductCreate/ProductCreate'
+import ProductEdit from './screens/ProductEdit/ProductEdit'
+import Detail from './screens/Detail/Detail'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import { verifyUser } from './services/users'
+import SignUp from './screens/SignUp/SignUp'
+import SignIn from './screens/SignIn/SignIn'
+import SignOut from './screens/SignOut/SignOut'
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser()
+      user ? setUser(user) : setUser(null)
+    }
+    fetchUser()
+  }, [])
+
   return (
-    <h1>hELLO</h1>
-  );
+    <div className="app">
+      <Switch>
+        <Route exact path="/">
+          <Home user={user} />
+        </Route>
+        <Route path="/sign-up">
+          <SignUp setUser={setUser} />
+        </Route>
+        <Route path="/sign-in">
+          <SignIn setUser={setUser} />
+        </Route>
+        <Route path="/sign-out">
+          <SignOut setUser={setUser} />
+        </Route>
+        <Route exact path="/products">
+          <Products user={user} />
+        </Route>
+        <Route path="/add-product">
+          {user ? <ProductCreate user={user} /> : <Redirect to="/sign-up" />}
+        </Route>
+        <Route exact path="/products/:id/edit">
+          {user ? <ProductEdit user={user} /> : <Redirect to='/' />}
+        </Route>
+        <Route exact path="/products/:id">
+          <Detail user={user} />
+        </Route>
+        <Route exact path="/support">
+          <Support user={user} />
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
-export default App;
+export default App
