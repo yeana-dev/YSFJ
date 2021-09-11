4cb5739b6b8c698db602d851d1b4bd1b670f737b
yeana0324 <yeana.dev@gmail.com>
2021-09-11 15:22:20 -0500

    Fixed wrong props on Detail

--- client/src/Components/ProductList/ProductList.jsx
+++ client/src/Components/ProductList/ProductList.jsx
@@ -1,36 +1,39 @@
-import { useState, useEffect } from 'react'
-import Detail from '../Detail/Detail'
-import { getProducts } from '../../services/products'
+import { useState, useEffect } from "react";
+import Detail from "../Detail/Detail";
+import { getProducts } from "../../Services/products";
 
 const ProductCards = () => {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const allProducts = await getProducts()
-      setProducts(allProducts)
-    }
-    fetchProducts()
-  }, [])
+      const allProducts = await getProducts();
+      setProducts(allProducts);
+    };
+    fetchProducts();
+  }, []);
 
+  console.log(products);
   const CARDS = products
     .reverse()
     .map((product, index) =>
       index < 8 ? (
         <Detail
           _id={product._id}
-          name={product.name}
-          imgURL={product.imgURL}
+          title={product.title}
+          imgURL={product.image_url}
+          color={product.color}
+          price={product.price}
           key={index}
         />
       ) : null
-    )
+    );
 
   return (
-    <div className='products'>
-      <div className='cards'>{CARDS}</div>
+    <div className="products">
+      <div className="cards">{CARDS}</div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCards
+export default ProductCards;
