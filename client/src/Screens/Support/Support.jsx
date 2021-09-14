44e45ac274c1c3f0198a73db6ab9306987377fc6
yeana0324 <yeana.dev@gmail.com>
2021-09-13 23:59:45 -0500

    Finished Nav CSS

--- client/src/Screens/Support/Support.jsx
+++ client/src/Screens/Support/Support.jsx
@@ -1,17 +1,50 @@
 import Layout from "../../Components/Layout/Layout";
 import SupportTeam from "../../Components/SupportTeam/SupportTeam";
+import Form from "react-bootstrap/Form";
+import "./Support.css";
 
-const Support = () => {
+const Support = (props) => {
   return (
-    <Layout>
-      <header>Support</header>
-      <div className="support-content">
-        Due to COVID-19, Please allow our customer service team to reach out up
-        to two business days.
-      </div>
-      <div className="support-team">
-        <p>To reach out our engineering team directly:</p>
-        <SupportTeam />
+    <Layout user={props.user}>
+      <div className="container">
+        <div className="support-left">
+          <header>SUPPORT</header>
+          <div className="support-content">
+            Due to COVID-19, Please allow our customer service team to reach out
+            up to two business days.
+          </div>
+          <div className="support-team">
+            <p>To reach out our engineering team directly:</p>
+            <SupportTeam />
+          </div>
+        </div>
+        <form className="support-right">
+          <Form.Floating className="mb-3">
+            <Form.Control
+              id="contact-name"
+              type="text"
+              placeholder="First / Last name"
+            />
+            <label htmlFor="contact-name">Name</label>
+          </Form.Floating>
+          <Form.Floating className="mb-3">
+            <Form.Control
+              id="contact-email"
+              type="email"
+              placeholder="name@example.com"
+            />
+            <label htmlFor="contact-email">E-mail</label>
+          </Form.Floating>
+          <Form.Floating className="mb-3">
+            <Form.Control
+              id="contact-message"
+              type="message"
+              placeholder="message"
+              as="textarea"
+            />
+            <label htmlFor="contact-message">Message</label>
+          </Form.Floating>
+        </form>
       </div>
     </Layout>
   );
