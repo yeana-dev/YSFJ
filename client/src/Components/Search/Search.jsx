84bed06b4be3ec5b74f9f8e7326817065168a4fe
yeana0324 <yeana.dev@gmail.com>
2021-09-15 18:50:09 -0500

    Added search bar css

--- client/src/Components/Search/Search.jsx
+++ client/src/Components/Search/Search.jsx
@@ -1,18 +1,19 @@
-import './Search.css'
+import "./Search.css";
 const Search = (props) => {
   return (
     <form className="search-form" onSubmit={(e) => props.onSubmit(e)}>
+      <i class="fas fa-search"></i>
       <input
         className="search-input"
         value={props.value}
         onChange={(e) => props.handleSearch(e)}
         name="Search"
-        placeholder="Search"
+        placeholder="SEARCH"
         type="text"
         autoFocus
       />
     </form>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
