dbb69a95048413682933e812e70a7e7dc63dd85c
Jorge Herrera <jorgeherrera@jorges-air.attlocal.net>
2021-09-16 09:51:05 -0500

    made a delete screen and add a banner for sales info

--- client/src/Components/Detail/Detail.jsx
+++ client/src/Components/Detail/Detail.jsx
@@ -3,32 +3,16 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const Detail = (props) => {
-  // const [colors, setColor] = useState("")
+
   const [selected, setSelected] = useState("")
+
 
 
   const handleSelect = (color) => {
     setSelected(color)
-    console.log(selected)
+
   }
 
-
-  // const handleColors = (selection) => {
-
-  //   return (
-  //     <>
-
-  //       <div className='color-circles'>{setColor(selection)}</div>
-  //       {colors.map((color) => (
-  //         <div className='bigger-circle'>{color}</div>
-  //       ))}
-
-
-  //     </>
-  //   )
-
-
-  // }
   return (
     <div className="detail-products">
       <Link className="card" to={`/products/${props._id}`}>
@@ -36,18 +20,22 @@ const Detail = (props) => {
           className="product-image"
           src={props.image_url}
           alt={props.title}
+
         />
         <div className='text-box'>
           <div className="product-title">{props.title}</div>
           <div className="price">{`$${props.price}`}</div>
           <div className='color-container'>
-            <div className="color">{`Select from ${props.color.length} colors:`} </div>
+            <div className="color">{`Select from these colors:`} </div>
           </div>
         </div>
       </Link>
       {props.color.map((color, index) => (
         <button style={{ backgroundColor: color }} className={`${selected === color ? `selected` : null}`} key={index} onClick={() => { handleSelect(color) }} ></button>)
-      )}
+      )
+
+      }
+
 
     </div >
   );
--- client/src/Screens/Products/Products.jsx
+++ client/src/Screens/Products/Products.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Card from 'react-bootstrap/Card';
 import "./Products.css";
 import Layout from "../../Components/Layout/Layout";
 import Detail from "../../Components/Detail/Detail";
@@ -13,7 +14,6 @@ function Products(props) {
     const fetchProducts = async () => {
       const allProducts = await getProducts();
       setProducts(allProducts);
-      console.log(allProducts);
       setSearchResult(allProducts);
     };
     fetchProducts();
@@ -24,9 +24,14 @@ function Products(props) {
       detail.title.toLowerCase().includes(event.target.value.toLowerCase())
     );
     setSearchResult(results);
+    console.log(searchResult.length)
   };
 
   const handleSubmit = (event) => event.preventDefault();
+
+  const firstHalfArr = searchResult.slice(0, 3);
+  const secondHalfArr = searchResult.slice(3);
+
 
   return (
     <Layout user={props.user}>
@@ -39,7 +44,48 @@ function Products(props) {
         <div className="middle-wrapper">
           <Search onSubmit={handleSubmit} handleSearch={handleSearch} />
           <div className="products">
-            {searchResult.map((product, index) => {
+
+
+            {
+              firstHalfArr.map((product, index) => {
+                return (
+                  <Detail
+                    _id={product._id}
+                    title={product.title}
+                    image_url={product.image_url[0]}
+                    price={product.price}
+                    color={product.color}
+                    key={index}
+                  />
+                );
+              })}
+
+
+            <Card id="sale-banner" >
+              <Card.Img id='horiz-image' src="https://images.unsplash.com/photo-1512793988391-0716d78a18ac?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fGJsYWNrJTIwYW5kJTIwd2hpdGUlMjBnbGFzc2VzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="card-image" />
+              <Card.ImgOverlay>
+                <Card.Text id='sale-text'>
+                  <h2 className='sale-message'>FOR LIMITED TIME YOU CAN GET SELECTED GLASSES FOR 25% OFF</h2>
+                  <p className='sale-message2'>OFFER ONLY VALID THRU 9-9 TO 10-11</p>
+                </Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+
+            {
+              secondHalfArr.map((product, index) => {
+                return (
+                  <Detail
+                    _id={product._id}
+                    title={product.title}
+                    image_url={product.image_url[0]}
+                    price={product.price}
+                    color={product.color}
+                    key={index}
+                  />
+                );
+              })}
+
+            {/* {searchResult.map((product, index) => {
               return (
                 <Detail
                   _id={product._id}
@@ -50,7 +96,9 @@ function Products(props) {
                   key={index}
                 />
               );
-            })}
+            })} */}
+
+
           </div>
         </div>
         <img
