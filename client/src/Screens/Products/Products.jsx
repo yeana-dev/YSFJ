4a82f811953fb131227b8ccf01186d5f4611975d
Jorge Herrera <jorgeherrera@jorges-air.attlocal.net>
2021-09-16 14:53:39 -0500

    Fixed products and added a sales banner

--- client/src/Screens/Products/Products.jsx
+++ client/src/Screens/Products/Products.jsx
@@ -65,8 +65,8 @@ function Products(props) {
               <Card.Img id='horiz-image' src="https://images.unsplash.com/photo-1512793988391-0716d78a18ac?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fGJsYWNrJTIwYW5kJTIwd2hpdGUlMjBnbGFzc2VzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="card-image" />
               <Card.ImgOverlay>
                 <Card.Text id='sale-text'>
-                  <h2 className='sale-message'>FOR LIMITED TIME YOU CAN GET SELECTED GLASSES FOR 25% OFF</h2>
-                  <p className='sale-message2'>OFFER ONLY VALID THRU 9-9 TO 10-11</p>
+                  <h2 className='sale-message' id='h2-sale'>FOR LIMITED TIME YOU CAN GET SELECTED GLASSES FOR 25% OFF</h2>
+                  <p className='sale-message2' id='p-sale' >OFFER ONLY VALID THRU 9-9 TO 10-11</p>
                 </Card.Text>
               </Card.ImgOverlay>
             </Card>
@@ -84,22 +84,8 @@ function Products(props) {
                   />
                 );
               })}
-
-            {/* {searchResult.map((product, index) => {
-              return (
-                <Detail
-                  _id={product._id}
-                  title={product.title}
-                  image_url={product.image_url[0]}
-                  price={product.price}
-                  color={product.color}
-                  key={index}
-                />
-              );
-            })} */}
-
-
           </div>
+
         </div>
         <img
           className="products-image2"
@@ -107,6 +93,7 @@ function Products(props) {
           alt="side-banner2"
         />
       </div>
+
     </Layout>
   );
 }
