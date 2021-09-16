3a93be36c58862d5208d2e329f9e0004689b1658
yeana0324 <yeana.dev@gmail.com>
2021-09-16 14:47:56 -0500

    Layout/Nav width changed. Added data. ProductEdit functionality

--- client/src/Components/Search/Search.jsx
+++ client/src/Components/Search/Search.jsx
@@ -2,7 +2,7 @@ import "./Search.css";
 const Search = (props) => {
   return (
     <form className="search-form" onSubmit={(e) => props.onSubmit(e)}>
-      <i class="fas fa-search"></i>
+      <i className="fas fa-search"></i>
       <input
         className="search-input"
         value={props.value}
--- seed/data.js
+++ seed/data.js
@@ -15,96 +15,143 @@ const insertData = async () => {
 
   const products = [
     {
-      title: "B",
+      title: "Yeana",
       image_url: [
-        "https://i.warbycdn.com/s/c/7f4f6dafeb2edceb16559a5f0856966674239d1f.png?quality=80&width=900",
-        "https://i.warbycdn.com/s/c/6db7c02bc7495d950cebd98d0147d0bc5047acac?quality=80&width=975",
-        "https://i.warbycdn.com/s/c/6db7c02bc7495d950cebd98d0147d0bc5047acac?quality=80&width=975",
+        "https://i.warbycdn.com/s/c/57983454e836700aecfbfb948246f9b27138af5d.png?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/a9ed50a94655db09cfa28129d82b37b7eb880c96?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/8effaddcc4c3a51040895fc88cf8a7305b2d3efc?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/73182b063052e9e834e423347482de0035ac1043?quality=80&width=900",
       ],
-      description: "description",
-      price: 45,
-      color: "black",
-      createdBy: "bruno"
+      description:
+        "Talk about versatile. Yeana's sort of square, sort of round lenses (and its subtly sculpted keyhole bridge) look great on a range of faces. It's made from hand-polished cellulose acetate and used Akulon-coated screws for durability.",
+      price: 99,
+      color: ["white", "black", "pink"],
+      createdBy: "bruno",
       //  category: {
       //   type: String,
       //   enum: ["glasses" "sunglasses"],
       // required: true, }: Post- MVP
     },
     {
-      title: "Avant-Garde",
+      title: "Sofanit",
       image_url: [
-        "https://i.warbycdn.com/s/c/fe448cc431fc333b1bf68886358ae45e08a55581.png?quality=80&width=900",
-        "https://i.warbycdn.com/s/c/6db7c02bc7495d950cebd98d0147d0bc5047acac?quality=80&width=975",
-        "https://i.warbycdn.com/s/c/6db7c02bc7495d950cebd98d0147d0bc5047acac?quality=80&width=975",
+        "https://i.warbycdn.com/s/c/92509c27839376565a59aea201d372d77d574ca6?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/df210bbbdcbc304d982c6a16fa8a9a4a8d0c4e77?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/645ea7099a0d288c53554ef42f4d550c41c98ee5?quality=80&width=900",
       ],
-      description: "description",
-      price: 45,
-      color: "black",
-      createdBy: "bruno"
+      description:
+        "Bold shape feels perfectly balanced thanks to its slightly square lenses and sloping browline. It's made from hand-polished cellulose acetate and used Akulon-coated screws for durability.",
+      price: 105,
+      color: ["black", "brown", "gold"],
+      createdBy: "bruno",
       //  category: {
       //   type: String,
       //   enum: ["glasses" "sunglasses"],
       // required: true, }: Post- MVP
     },
     {
-      title: "Avant-Garde",
+      title: "Francisco",
       image_url: [
-        "https://i.warbycdn.com/s/c/5985e18cf8f10fbdd719fc7945b2a7a51a9b0393.png?quality=80&width=900",
-        "https://i.warbycdn.com/s/c/6db7c02bc7495d950cebd98d0147d0bc5047acac?quality=80&width=975",
-        "https://i.warbycdn.com/s/c/6db7c02bc7495d950cebd98d0147d0bc5047acac?quality=80&width=975",
+        "https://i.warbycdn.com/s/c/30bc42d5ab039c7e6d3f0b060a1f40f579a27d39?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/e33e35829d2395b3cae69a780056792537686b3c?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/cc528983e9773dae4a921f4acbb25f0d254897b3?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/879d7e538ef6c30e3ec882af7b8414b769d18ebc?quality=80&width=900",
       ],
-      description: "description",
-      price: 45,
-      color: "black",
-      createdBy: "bruno"
+      description:
+        "Look closely at Francisco's charmingly round eyewires, and you'll see they're encircled by ultra-thin acetate coils. The effect? Delicate but distinguished, and made possible by our partners in northern Italy. It's made from hand-polished cellulose acetate and used Akulon-coated screws for durability.",
+      price: 115,
+      color: ["gold", "silver"],
+      createdBy: "bruno",
       //  category: {
       //   type: String,
       //   enum: ["glasses" "sunglasses"],
       // required: true, }: Post- MVP
     },
     {
-      title: "Avant-Garde",
+      title: "Jorge",
       image_url: [
-        "https://i.warbycdn.com/s/c/44b20d76a68ea48411406d1fcd326d01a6051545?quality=75&width=900",
-        "https://i.warbycdn.com/s/c/6db7c02bc7495d950cebd98d0147d0bc5047acac?quality=80&width=975",
-        "https://i.warbycdn.com/s/c/6db7c02bc7495d950cebd98d0147d0bc5047acac?quality=80&width=975",
+        "https://i.warbycdn.com/s/c/502af193a6683d3d757c95527169784d45a05db4?quality=75&width=900",
+        "https://i.warbycdn.com/s/c/7481418af166e4cf8a3b234545d44540754e778f?quality=80&width=975",
+        "https://i.warbycdn.com/s/c/3495c6525be253ebf49a3f710627955d3c473ec7?quality=80&width=975",
+        "https://i.warbycdn.com/s/c/4397a78306c62e1a89adecc904903170f337dd01?quality=80&width=975",
       ],
-      description: "description",
-      price: 45,
-      color: "black",
-      createdBy: "bruno"
+      description:
+        "With Jorge, grand proportions (oversized round lenses, anyone?) meet thinner acetate for an airier, softer look. Balance is key, after all. It's made from hand-polished cellulose acetate and used Akulon-coated screws for durability.",
+      price: 105,
+      color: ["black", "brown", "darkgreen"],
+      createdBy: "bruno",
       //  category: {
       //   type: String,
       //   enum: ["glasses" "sunglasses"],
       // required: true, }: Post- MVP
     },
     {
-      title: "Avant-Garde",
+      title: "Bruno",
       image_url: [
-        "https://i.warbycdn.com/s/c/7c495d1a5c4552e04842ad2875080a8ea034ac0a?quality=75&width=900",
-        "https://i.warbycdn.com/s/c/6db7c02bc7495d950cebd98d0147d0bc5047acac?quality=80&width=975",
-        "https://i.warbycdn.com/s/c/6db7c02bc7495d950cebd98d0147d0bc5047acac?quality=80&width=975",
+        "https://i.warbycdn.com/s/c/be1f9b339a08e5c1a7821ecb8d8e54b4edbe2823?quality=75&width=900",
+        "https://i.warbycdn.com/s/c/8e8775cea8dc712328d10db147cb78ea5e39738b?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/ff5d671952decff591fc0caff9bf952ed57613c8?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/e2e1c6fc9ac25924935755b9f7ec05323d0f99d2?quality=80&width=900",
       ],
-      description: "description",
-      price: 45,
-      color: "black",
-      createdBy: "bruno"
+      description:
+        "Bruno is a roomier, round frame with some unexpected edge. See its angular, squared-off bottom? That's it! It's made from hand-polished cellulose acetate and used Akulon-coated screws for durability.",
+      price: 95,
+      color: ["black", "brown", "white"],
+      createdBy: "bruno",
       //  category: {
       //   type: String,
       //   enum: ["glasses" "sunglasses"],
       // required: true, }: Post- MVP
     },
     {
-      title: "Avant-Garde",
+      title: "Nora",
       image_url: [
-        "https://i.warbycdn.com/s/c/3c477a65ccd053353cf10a5fdd67110f383934c8?quality=75&width=900",
-        "https://i.warbycdn.com/s/c/6db7c02bc7495d950cebd98d0147d0bc5047acac?quality=80&width=975",
-        "https://i.warbycdn.com/s/c/6db7c02bc7495d950cebd98d0147d0bc5047acac?quality=80&width=975",
+        "https://i.warbycdn.com/s/c/898edad196c145a62d62f8e47ab6d7d78448f755?quality=75&width=900",
+        "https://i.warbycdn.com/s/c/3ec7203c84b4766d08d31993f5911d82e61bf71e?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/73269cc2a17a1e8602fc3b607d882acdd5a325f2?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/3c834cdcc0e91221cbe558b7afe9909f38ba7c2d?quality=80&width=900",
       ],
-      description: "description",
-      price: 45,
-      color: "black",
-      createdBy: "bruno"
+      description:
+        "What do you get when you combine round lenses, slim temple arms, and a keyhole bridge? A good-looking frame named Nora. It's made from hand-polished cellulose acetate and used Akulon-coated screws for durability.",
+      price: 95,
+      color: ["black", "brown", "darkgreen", "white"],
+      createdBy: "bruno",
+      //  category: {
+      //   type: String,
+      //   enum: ["glasses" "sunglasses"],
+      // required: true, }: Post- MVP
+    },
+    {
+      title: "Raul",
+      image_url: [
+        "https://i.warbycdn.com/s/c/c2f5125da30588f75945ad87c4308a333e408b05?quality=75&width=900",
+        "https://i.warbycdn.com/s/c/4d06605e46e0677a9c62cb2cb71303c142958955?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/9d39be2ae9b38a80b7db55a9b1faa4cb71bf3256?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/9b51e19e083ed93ebe3d4907ba67f9dece770fc6?quality=80&width=900",
+      ],
+      description:
+        "Two slightly ’70s touches—an oversized fit and an almost-flat browline—give Raul an easygoing appeal. It's made from hand-polished cellulose acetate and used Akulon-coated screws for durability.",
+      price: 95,
+      color: ["black", "brown"],
+      createdBy: "bruno",
+      //  category: {
+      //   type: String,
+      //   enum: ["glasses" "sunglasses"],
+      // required: true, }: Post- MVP
+    },
+    {
+      title: "Nikki",
+      image_url: [
+        "https://i.warbycdn.com/s/c/7cc058eb3f5c4699bf1a235e07b76d1db27ef800?quality=75&width=900",
+        "https://i.warbycdn.com/s/c/c129b46327f096abfc6c078ea6bb7e6800eb0a58?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/d5bfa43e63dc5227a5a598badf19d0ad03ba5deb?quality=80&width=900",
+        "https://i.warbycdn.com/s/c/191527ac2d7cb029db3638f2b3ff330405e5b1c5?quality=80&width=900",
+      ],
+      description:
+        "Nikki, one of our tried-and-true classics, gets slightly revamped with Low Bridge Fit construction. It's made from hand-polished cellulose acetate and used Akulon-coated screws for durability.",
+      price: 95,
+      color: ["brown", "pink"],
+      createdBy: "bruno",
       //  category: {
       //   type: String,
       //   enum: ["glasses" "sunglasses"],
