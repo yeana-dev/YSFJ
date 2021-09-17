cd6565b1ca08817e0b1ebf914b5b4f3086278da8
FranciscoPink <franciscorosadomunoz@icloud.com>
2021-09-17 10:17:19 -0400

    updating code

--- controllers/users.js
+++ controllers/users.js
@@ -116,7 +116,7 @@ export const deleteCartItem = async (req, res) => {
 export const addToCart = async (req, res) => {
   try {
     if (await User.findById(req.params.id)) {
-      const product = await Product.findByIdAndUpdate(userId: user._id, req.body, { new: true })
+      const product = await Product.findByIdAndUpdate({ userId: User._id }, req.body, { new: true })
       res.status(200).json(product)
     }
     throw new Error(`User ${req.params.id} does not exist!`)
