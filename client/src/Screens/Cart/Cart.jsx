158289a0ed7c9cc96792fb14ec0deba31da59b47
yeana0324 <yeana.dev@gmail.com>
2021-10-21 15:01:39 -0500

    Fixed bug where updated cart item not showing up

--- client/src/Screens/Cart/Cart.jsx
+++ client/src/Screens/Cart/Cart.jsx
@@ -1,53 +1,53 @@
 import { useState, useEffect } from "react";
 import "./Cart.css";
 import Layout from "../../Components/Layout/Layout";
-import { getCart } from "../../Services/users";
-import { deleteCartItem } from "../../Services/users";
+import { getProducts } from "../../Services/products";
+import { getCart, deleteCartItem } from "../../Services/users";
 
 function Cart(props) {
-  const [products, setProducts] = useState([]);
-  const [searchResult, setSearchResult] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const allProducts = await getCart(props.user.id);
-      setProducts(allProducts);
-      console.log(allProducts);
-      setSearchResult(allProducts);
+    const fetchCart = async () => {
+      const items = await getCart(props.user.id);
+      const allProducts = await getProducts();
+      let cartArray = [];
+      items.forEach((item) => {
+        cartArray.push(allProducts.find((product) => item === product._id));
+      });
+      setCartItems(cartArray);
     };
-    fetchProducts();
+    fetchCart();
   }, [props.user.id]);
 
   let totalPrice = 0;
+  console.log(cartItems);
 
   return (
     <Layout user={props.user}>
       <div className="cart-container">
-        {searchResult.map((product) => {
+        {cartItems.map((product) => {
+          console.log(product);
           totalPrice += product.price;
           return (
             <div className="cart-item">
               <div className="cart-item-left">
                 <div className="cart-item-title">{product.title}</div>
-                <div className="cart-item-price">${product.price}</div>
+                <div className="cart-item-price">$ {product.price}</div>
                 <hr />
                 <div className="cart-item-description">
                   {product.description}
                 </div>
               </div>
               <img src={product.image_url[0]} alt="cart-item-preview" />
-              <button
-                onClick={() => {
-                  deleteCartItem(product.userId, product._id);
-                }}
-              >
+              <button onClick={() => {}}>
                 <i class="fas fa-trash-alt"></i>
               </button>
             </div>
           );
         })}
         <div className="cart-item">
-          <div className="cart-total-price">Total : ${totalPrice}</div>
+          <div className="cart-total-price">Total : $ {totalPrice}</div>
         </div>
       </div>
     </Layout>
