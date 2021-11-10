4363d6066743bac5468036874194357408034a36
yeana0324 <yeana.dev@gmail.com>
2021-11-10 13:23:48 -0600

    Added getUsers controller

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,8 @@ router.post("/sign-up", controllers.signUp);
 router.post("/sign-in", controllers.signIn);
 router.get("/verify", controllers.verify);
 
+router.get("/users", controllers.getUsers);
 router.get("/users/:id", controllers.getUser);
-
 router.get("/users/:id/cart", controllers.getCart);
 router.delete("/users/:id/cart/:productId", controllers.deleteCartItem);
 router.put("/users/:id/cart/:productId", controllers.addToCart);
